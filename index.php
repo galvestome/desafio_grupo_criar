765b6cdfe0ec9f27a922786dc28eef801507834f
galvestome <gstv.stz@gmail.com>
2023-06-01 15:30:07 -0300

    criacao e configuracao inicial do html, css e classe principal

--- index.php
+++ index.php
@@ -5,8 +5,23 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Corrida de Kart</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="assets/css/index.css">
 </head>
 <body>
+
+  <div class="container">
+    <h2>Envie abaixo o log da corrida para processar as informacoes</h2>
+    <span>* Permitido apenas extensoes .txt e .log</span>
+    <form method="post" enctype="multipart/form-data" action="info.php">
+      <div class="input-group mb-3">
+        <input type="file" class="form-control" name="logCorrida" id="logCorrida">
+        <label class="input-group-text" for="logCorrida">Upload</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Teste</button>
+    </form>
+  </div>
   
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ info.php
@@ -0,0 +1,33 @@
+<?php 
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  spl_autoload_register(function($className){
+    include 'src/'.$className.'.php';
+  });
+
+  print_r($_FILES['logCorrida']);
+
+  $up = new upload();
+
+} else{
+    header("Location: index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Informacoes da Corrida</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="assets/css/info.css">
+</head>
+<body>
+
+  <h1>Informacoes da Corrida</h1>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
